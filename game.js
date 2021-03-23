04eb2b296a7520d9b43b049516592da250425326
joaov <joaov@kyros.com.br>
2021-03-22 22:05:44 -0300

    first commit

---
+++ game.js
@@ -0,0 +1,410 @@
+import mapping from './mapping.js'
+
+export default function startGame(initialSize) {
+	//cria a matriz
+	let game = {
+		size: initialSize,
+		grid: new Array(initialSize),
+		score: 0,
+		observers: []
+	}
+
+	let previousState = {
+		grid: new Array(initialSize),
+		score: 0
+	};
+
+	function subscribe(observerFunction) {
+		game.observers.push(observerFunction);
+		observerFunction({ size: game.size, grid: game.grid, score: game.score });
+	}
+
+	function notifyAll(command) {
+		for (const observerFunction of game.observers) {
+			observerFunction(command);
+		}
+	}
+
+	function resize(newSize) {
+		game.size = newSize;
+		game.score = 0;
+		game.grid = new Array(game.size);
+		previousState.grid = new Array(game.size);
+		previousState.score = 0;
+
+		for (let position = 0; position < game.size; position++) {
+			game.grid[position] = new Array(game.size);
+			game.grid[position].fill(0);
+
+			previousState.grid[position] = new Array(game.size);
+			previousState.grid[position].fill(0);
+		}
+
+		notifyAll({
+			size: game.size, grid: game.grid, score: game.score,
+			type: 'newGame'
+		});
+
+		let position = newBlock(game);
+		notifyAll({
+			size: game.size, grid: game.grid, score: game.score,
+			type: 'appear', in: { x: position.x, y: position.y}, value: game.grid[position.x][position.y]
+		});
+
+		position = newBlock(game);
+		notifyAll({
+			size: game.size, grid: game.grid, score: game.score,
+			type: 'appear', in: { x: position.x, y: position.y}, value: game.grid[position.x][position.y]
+		});
+
+		//notifyAll({ size: game.size, grid: game.grid, score: game.score });
+	}
+
+	function move(input) {
+		let hasUpdated = false;
+		let firstMove = true;
+
+		if (input == 'backspace') {
+			notifyAll({
+				size: game.size, grid: game.grid, score: game.score,
+				type: 'newGame'
+			});
+
+			for (let line in game.grid) {
+				for (let column in game.grid[line]) {
+					game.grid[line][column] = previousState.grid[line][column];
+					
+					notifyAll({
+						size: game.size, grid: game.grid, score: game.score,
+						type: 'appear', in: { x: line, y: column}, value: game.grid[line][column]
+					});
+				}
+			}
+
+			game.score = previousState.score;
+
+			//notifyAll({ size: game.size, grid: game.grid, score: game.score });
+		}
+
+		function saveState() {
+			for (let line in game.grid) {
+				for (let column in game.grid[line]) {
+					previousState.grid[line][column] = game.grid[line][column];
+				}
+			}
+
+			previousState.score = game.score;
+		}
+
+		stackUp(input);
+		join(input);
+		stackUp(input);
+
+		function swapBlocks(x1, y1, x2, y2) {
+			if (firstMove) {
+				saveState();
+				firstMove = false;
+				hasUpdated = true;
+			}
+
+			const aux = game.grid[x1][y1];
+			game.grid[x1][y1] = game.grid[x2][y2];
+			game.grid[x2][y2] = aux;
+
+			notifyAll({
+				size: game.size, grid: game.grid, score: game.score,
+				type: 'move', from: { x: x2, y: y2 }, to: { x: x1, y: y1 }, value: game.grid[x1][y1]
+			});
+		}
+
+		function joinBlocks(xIn, yIn, xErase, yErase) {
+			if (firstMove) {
+				saveState();
+				firstMove = false;
+				hasUpdated = true;
+			}
+
+			game.grid[xIn][yIn] += 1;
+			game.grid[xErase][yErase] = 0;
+			game.score += 2 ** game.grid[xIn][yIn];
+
+			notifyAll({
+				size: game.size, grid: game.grid, score: game.score,
+				type: 'join', to: { x: xIn, y: yIn }, from: { x: xErase, y: yErase }, value: game.grid[xIn][yIn] - 1
+			});
+		}
+
+		if (hasUpdated) {
+			const position = newBlock(game);
+			notifyAll({
+				size: game.size, grid: game.grid, score: game.score,
+				type: 'appear', in: { x: position.x, y: position.y}, value: game.grid[position.x][position.y]
+			});
+		}
+
+		function stackUp(direction) {
+			const stackFunctions = {
+				up() {
+					let position = 0;
+
+					for (let line in game.grid) {
+						if (Number(line) - 1 < 0) {
+							continue;
+						}
+
+						for (let column in game.grid[line]) {
+							position = Number(line);
+
+							if (game.grid[position][column] == 0) {
+								continue;
+							}
+
+							while (position > 0) {
+								if (game.grid[position - 1][column] == 0) {
+									swapBlocks(position - 1, column, position, column);
+									position -= 1;
+								} else {
+									break;
+								}
+							}
+						}
+					}
+				},
+				down() {
+					let position = 0;
+
+					for (let line in game.grid) {
+						if (Number(line) == 0) {
+							continue
+						}
+
+						for (let column in game.grid[line]) {
+							position = game.size - 1 - Number(line);
+
+							if (game.grid[position][column] == 0) {
+								continue;
+							}
+
+							while (position < game.size - 1) {
+								if (game.grid[position + 1][column] == 0) {
+									swapBlocks(position + 1, column, position, column);
+									position += 1;
+								} else {
+									break;
+								}
+							}
+						}
+					}
+				},
+				right() {
+					let position = 0;
+
+					for (let line in game.grid) {
+						for (let column in game.grid[line]) {
+							if (Number(column) == 0) {
+								continue;
+							}
+
+							position = game.size - 1 - Number(column);
+
+							if (game.grid[line][position] == 0) {
+								continue;
+							}
+
+							while (position < game.size - 1) {
+								if (game.grid[line][position + 1] == 0) {
+									swapBlocks(line, position + 1, line, position);
+									position += 1;
+								} else {
+									break;
+								}
+							}
+						}
+					}
+				},
+				left() {
+					let position = 0;
+
+					for (let line in game.grid) {
+						for (let column in game.grid[line]) {
+							if (Number(column) - 1 < 0) {
+								continue;
+							}
+
+							position = Number(column);
+
+							if (game.grid[line][position] == 0) {
+								continue;
+							}
+
+							while (position > 0) {
+								if (game.grid[line][position - 1] == 0) {
+									swapBlocks(line, position - 1, line, position);
+									position -= 1;
+								} else {
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			if (stackFunctions[direction]) {
+				return stackFunctions[direction]();
+			}
+		}
+
+		function join(direction) {
+			const joinFunctions = {
+				up() {
+					let next = 0;
+
+					for (let line in game.grid) {
+						next = Number(line) + 1;
+
+						if (next == game.size) {
+							break;
+						}
+
+						for (let column in game.grid[line]) {
+							if (game.grid[line][column] == 0) {
+								continue;
+							}
+
+							if (game.grid[line][column] == game.grid[next][column]) {
+								joinBlocks(line, column, next, column);
+							}
+						}
+					}
+				},
+				down() {
+					let next = 0;
+					let position = 0;
+
+					for (let line in game.grid) {
+						position = game.size - 1 - Number(line);
+						next = position - 1;
+
+						if (next < 0) {
+							break;
+						}
+
+						for (let column in game.grid[line]) {
+							if (game.grid[position][column] == 0) {
+								continue;
+							}
+
+							if (game.grid[position][column] == game.grid[next][column]) {
+								joinBlocks(position, column, next, column);
+							}
+						}
+					}
+				},
+				right() {
+					let next = 0;
+					let position = 0;
+
+					for (let line in game.grid) {
+						for (let column in game.grid[line]) {
+							position = game.size - 1 - Number(column);
+							next = position - 1;
+
+							if (next < 0) {
+								break;
+							}
+
+							if (game.grid[line][position] == 0) {
+								continue;
+							}
+
+							if (game.grid[line][position] == game.grid[line][next]) {
+								joinBlocks(line, position, line, next);
+							}
+						}
+					}
+				},
+				left() {
+					let next = 0;
+
+					for (let line in game.grid) {
+						for (let column in game.grid[line]) {
+							next = Number(column) + 1;
+
+							if (next == game.size) {
+								break;
+							}
+
+							if (game.grid[line][column] == 0) {
+								continue;
+							}
+
+							if (game.grid[line][column] == game.grid[line][next]) {
+								joinBlocks(line, column, line, next);
+							}
+						}
+					}
+				}
+			}
+
+			if (joinFunctions[direction]) {
+				return joinFunctions[direction]();
+			}
+		}
+	}
+
+	return {
+		subscribe,
+		resize,
+		move
+	};
+}
+
+function newBlock(gameObj) {
+	//escolher uma posição vazia (= 0)
+	const position = sortEmptyBlock(gameObj);
+
+	//sortear um numero para a posição (1 ou 2)
+	if (position) {
+		//logica para 10% de chance de surgir um 4
+		if (sortNewValue(0, 10) < 9) {
+			gameObj.grid[position.x][position.y] = 1;
+		} else {
+			gameObj.grid[position.x][position.y] = 2;
+		}
+
+		return position;
+	}
+}
+
+function sortEmptyBlock(gameObj) {
+	const position = { x: 0, y: 0 };
+
+	if (hasEmptyBlocks(gameObj.grid)) {
+		do {
+			position.x = sortNewValue(0, gameObj.size);
+			position.y = sortNewValue(0, gameObj.size);
+
+		} while (gameObj.grid[position.x][position.y] != 0);
+
+		return position;
+	} else {
+		return null;
+	}
+}
+
+function hasEmptyBlocks(matrice) {
+	for (let line in matrice) {
+		for (let column in matrice) {
+			if (matrice[line][column] == 0) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function sortNewValue(min, max) {
+	return Math.floor(mapping(Math.random(), 0, 1, min, max));
+}
---
+++ graphic.js
@@ -0,0 +1,258 @@
+import mapping from './mapping.js'
+
+//Exibição
+export default function newGraphicCanvas(windowInput, canvasId) {
+	const canvas = windowInput.document.getElementById(canvasId);
+	const screen = canvas.getContext("2d");
+
+	const state = {
+		size: 0,
+		grid: []
+	}
+
+	let animationsList = [];
+
+	function stateUpdate(gameStateObj) {
+		if (state.size != gameStateObj.size) {
+			state.size = gameStateObj.size;
+
+			state.grid = new Array(state.size);
+		}
+
+		if (gameStateObj.type) {
+			const moveTypes = {
+				move() {
+					animationsList.push({
+						type: gameStateObj.type,
+						from: gameStateObj.from,
+						to: gameStateObj.to,
+						value: gameStateObj.value,
+						progress: 0
+					});
+				},
+				join() {
+					animationsList.push({
+						type: gameStateObj.type,
+						from: gameStateObj.from,
+						to: gameStateObj.to,
+						value: gameStateObj.value,
+						progress: 0
+					});
+				},
+				appear() {
+					animationsList.push({
+						type: gameStateObj.type,
+						in: gameStateObj.in,
+						value: gameStateObj.value,
+						progress: 0
+					});
+				},
+				newGame() {
+					for (let position = 0; position < state.size; position++) {
+						state.grid[position] = new Array(state.size);
+						state.grid[position].fill(0);
+					}
+				}
+			}
+
+			if(moveTypes[gameStateObj.type]) {
+				moveTypes[gameStateObj.type]();
+			}
+		}
+	}
+
+	//desenha o novo frame
+	function newFrame() {
+		//limpa a tela
+		screen.clearRect(0, 0, canvas.width, canvas.height);
+
+		const block = {
+			size: null,
+			space: null
+		}
+
+		block.space = 80 / state.size;
+
+		block.size = {
+			width: (canvas.width - (state.size * block.space) - block.space) / state.size,
+			height: (canvas.height - (state.size * block.space) - block.space) / state.size
+		}
+
+		screen.textBaseline = 'middle';
+		screen.textAlign = 'center';
+
+		const color = {
+			background: 'darkgray',
+			emptyBlock: 'white',
+			notEmptyBlock(value) {
+				return `rgb(
+                    ${(2 ** value <= 2048) ? (255) : (0)},
+                    ${(2 ** value <= 2048) ? (200 - Math.floor(mapping(value, 0, Math.log2(2048), 0, 200))) : (0)},
+                    ${(2 ** value <= 2048) ? (200 - Math.floor(mapping(value, 0, Math.log2(2048), 0, 200))) : (0)}
+                )`;
+			},
+			text: 'white'
+		}
+
+		screen.fillStyle = "darkgray";
+		screen.fillRect(0, 0, canvas.width, canvas.height);
+
+		for (let line in state.grid) {
+			for (let column in state.grid[line]) {
+				printBlock(line, column, state.grid[line][column]);
+			}
+		}
+
+		runAnimations();
+
+		function runAnimations() {
+			const animation = animationsList[0];
+			const animationStep = 20;
+
+			const moves = {
+				move() {
+					//printa o bloco em movimento
+					if (animation.to.x != animation.from.x) {
+						//vertical
+						let progressRelation = (animation.to.x > animation.from.x) ? (animation.progress) : (100 - animation.progress);
+						let blocksToMove = (Math.abs(animation.to.x - animation.from.x)) * (progressRelation) / 100;
+						let absoluteBlockPosition = (animation.to.x > animation.from.x) ? (animation.from.x + blocksToMove) : (animation.to.x + blocksToMove);
+
+						printBlock(absoluteBlockPosition, animation.to.y, animation.value);
+					} else {
+						//horizontal
+						let progressRelation = (animation.to.y > animation.from.y) ? (animation.progress) : (100 - animation.progress);
+						let blocksToMove = (Math.abs(animation.to.y - animation.from.y)) * (progressRelation) / 100;
+						let absoluteBlockPosition = (animation.to.y > animation.from.y) ? (animation.from.y + blocksToMove) : (animation.to.y + blocksToMove);
+
+						printBlock(animation.to.x, absoluteBlockPosition, animation.value);
+					}
+				},
+				join() {
+					//printa o bloco em movimento
+					if (animation.to.x != animation.from.x) {
+						//vertical
+						let progressRelation = (animation.to.x > animation.from.x) ? (animation.progress) : (100 - animation.progress);
+						let blocksToMove = (Math.abs(animation.to.x - animation.from.x)) * (progressRelation) / 100;
+						let absoluteBlockPosition = (animation.to.x > animation.from.x) ? (animation.from.x + blocksToMove) : (animation.to.x + blocksToMove);
+
+						printBlock(absoluteBlockPosition, animation.to.y, animation.value);
+					} else {
+						//horizontal
+						let progressRelation = (animation.to.y > animation.from.y) ? (animation.progress) : (100 - animation.progress);
+						let blocksToMove = (Math.abs(animation.to.y - animation.from.y)) * (progressRelation) / 100;
+						let absoluteBlockPosition = (animation.to.y > animation.from.y) ? (animation.from.y + blocksToMove) : (animation.to.y + blocksToMove);
+
+						printBlock(animation.to.x, absoluteBlockPosition, animation.value);
+					}
+
+					//printa o bloco novo
+					printBlock(animation.to.x, animation.to.y, animation.value);
+
+					let size = block.size.width + 2 * block.space;
+
+					//penultimo passo
+					if (animation.progress + (3 * animationStep) >= 100) {
+						printBlock(animation.to.x, animation.to.y, animation.value, size, size);
+					}
+
+					//ultimo passo
+					if (animation.progress + animationStep >= 100) {
+
+						printBlock(animation.to.x, animation.to.y, animation.value + 1, size, size);
+					}
+				},
+				appear() {
+					let size = block.size.width * animation.progress / 100;
+					printBlock(animation.in.x, animation.in.y, animation.value, size, size);
+				}
+			}
+
+			const begin = {
+				move(animationObj) {
+					state.grid[animationObj.from.x][animationObj.from.y] = 0;
+				},
+				join(animationObj) {
+					state.grid[animationObj.from.x][animationObj.from.y] = 0;
+				},
+				appear(animationObj) {
+
+				}
+			}
+
+			const finish = {
+				move(animationObj) {
+					state.grid[animationObj.to.x][animationObj.to.y] = animationObj.value;
+				},
+				join(animationObj) {
+					state.grid[animationObj.to.x][animationObj.to.y] = animationObj.value + 1;
+				},
+				appear(animationObj) {
+					state.grid[animationObj.in.x][animationObj.in.y] = animationObj.value;
+				}
+			}
+
+			if (animation) {
+				if (animation.progress == 0) {
+					begin[animation.type](animation);
+				}
+
+				if (moves[animation.type]) {
+					moves[animation.type]();
+				}
+
+				animation.progress += animationStep;
+
+				if (animation.progress >= 100) {
+					console.log(`animated ${animationsList[0].type}`);
+					finish[animation.type](animation);
+					animationsList.shift();
+				}
+			}
+		}
+
+		function printBlock(x, y, value, width = block.size.width, height = block.size.height) {
+			let printParam = {
+				init: {
+					x: block.space + y * (block.size.width + block.space),
+					y: block.space + x * (block.size.height + block.space)
+				},
+				size: block.size,
+				textPosition: null
+			}
+
+			printParam.textPosition = {
+				x: printParam.init.x + (printParam.size.width / 2),
+				y: printParam.init.y + (printParam.size.height / 2)
+			}
+
+			if (value != 0) {
+				screen.fillStyle = color.notEmptyBlock(value);
+
+				printParam.init.x += (block.size.width / 2) - (width / 2);
+				printParam.init.y += (block.size.height / 2) - (height / 2);
+
+				screen.fillRect(printParam.init.x, printParam.init.y, width, height);
+
+				screen.fillStyle = color.text;
+				if (String(2 ** value).length < 3) {
+					screen.font = `bold ${(2 / 3) * height}px Arial`;
+					screen.fillText(String(2 ** value), printParam.textPosition.x, printParam.textPosition.y);
+				} else {
+					screen.font = `bold ${(2 / String(2 ** value).length) * (2 / 3) * height}px Arial`;
+					screen.fillText(String(2 ** value), printParam.textPosition.x, printParam.textPosition.y);
+				}
+			} else {
+				screen.fillStyle = color.emptyBlock;
+				screen.fillRect(printParam.init.x, printParam.init.y, printParam.size.width, printParam.size.height);
+			}
+		}
+
+		windowInput.requestAnimationFrame(newFrame);
+	}
+
+	return {
+		stateUpdate,
+		newFrame
+	}
+}
