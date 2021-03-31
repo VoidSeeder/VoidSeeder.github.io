fd61d438ef027cb3ce9b07a17a3f685bc47b3cc1
joaov <joaov@kyros.com.br>
2021-03-30 21:31:46 -0300

    adicionados efeitos sonoros

---
+++ Tetris/sounds.js
@@ -0,0 +1,32 @@
+export default function newSound() {
+	const soundEffects = {	
+		fullLine: new Audio('./soundEffects/mixkit-explainer-video-game-alert-sweep-236.wav'),
+		scoreUpdate: new Audio('./soundEffects/mixkit-retro-arcade-casino-notification-211.wav')
+	}
+
+	for (let audio in soundEffects) {
+		soundEffects[audio].stop = function () {
+			this.pause();
+			this.currentTime = 0;
+		}
+	}
+
+	function soundObserver (command) {
+		for (let line in command.state) {
+			if(!command.state[line].includes('empty')) {
+				soundEffects.fullLine.stop();
+				soundEffects.fullLine.play();
+				break;
+			}
+		}
+
+		if(command.score > 0) {
+			soundEffects.fullLine.stop();
+			soundEffects.scoreUpdate.play();
+		}
+	}
+
+	return {
+		soundObserver
+	}
+}
